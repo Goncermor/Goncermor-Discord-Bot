0a90ce8b60336cdf880e08757c463ba8c2361484
Gonçalo Morais <goncalocerto9morais@gmail.com>
2022-08-15 15:54:59 +0100

    Added ping example command

---
+++ example-commands/commands/ping.js
@@ -0,0 +1,32 @@
+//    ______
+//   / ____/___  ____  ________  _________ ___  ____  _____
+//  / / __/ __ \/ __ \/ ___/ _ \/ ___/ __ `__ \/ __ \/ ___/
+// / /_/ / /_/ / / / / /__/  __/ /  / / / / / / /_/ / /
+// \____/\____/_/ /_/\___/\___/_/  /_/ /_/ /_/\____/_/
+
+// Command plugin example
+
+module.exports = {
+    Command: "ping",                  // the command
+    Aliases: ["pg"],                  // aliases for the command (alternative commands)
+    Description: "Returns pong",      // description of the command
+    Value: "",                        // value name if any 
+    Slash: {
+        CreateCommand: false,         // true if you want to make a / command
+        Type: 0,                      // type of value (0 for none)
+        Required: false               // if the value is required
+    },
+
+    prefixrun: async (client, message, args) => {
+        // place here the code for the command with prefix
+        message.reply("Pong!");
+    },
+
+    slashrun: async (client, interaction, args) => {
+        // place here the code for the command with slash
+        // interaction.reply("this is an example");
+    },
+};
+
+
+// To know how to write code for this https://discord.js.org/docs#/docs/discord.js/main/general/welcome
